29d25bf9d1944850c4a63cc638e65186ffa9b194
bugCreator6999 <bugcreator@bugcreators-cafe.com>
2023-06-27 23:28:53 +0900

    modify css in blog detail page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -7,6 +7,16 @@ import { format } from "date-fns";
 import styles from "../../styles/page.module.css";
 import Link from "next/link";
 
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
+
+import { M_PLUS_Rounded_1c } from "next/font/google";
+
+const mPlus400 = M_PLUS_Rounded_1c({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default async function BlogDetail({
   params: { id },
 }: {
@@ -15,23 +25,32 @@ export default async function BlogDetail({
   const blogDetail: BlogPostType = await getBlogDetail(id);
   return (
     <>
-      <div className="title">
-        <h1>{blogDetail.title}</h1>
-      </div>
-      <div className="createdate">
-        <p>{format(new Date(blogDetail.createdAt), "yyyy-MM-dd HH:mm")}</p>
-      </div>
-      <div className="detailImage">
-        <Image
-          src={blogDetail.eyecatch?.url || "/no-image.png"}
-          width={320}
-          height={200}
-          alt={blogDetail.title}
-        />
-      </div>
-      <div className="detail">{parse(blogDetail.content)}</div>
-      <div className={styles.detailBack}>
-        <Link href="/blog">戻る</Link>
+      <div className={mPlus400.className}>
+        <div className={styles.detailArea}>
+          <div className={styles.detailTitle}>
+            <h1>{blogDetail.title}</h1>
+          </div>
+          <div className={styles.detailDate}>
+            <p>{format(new Date(blogDetail.createdAt), "yyyy年MM月dd日")}</p>
+          </div>
+          <div className={styles.detailImage}>
+            <Image
+              src={blogDetail.eyecatch?.url || "/no-image.png"}
+              //   width={320}
+              //   height={200}
+              layout="fill"
+              objectFit="contain"
+              alt={blogDetail.title}
+            />
+          </div>
+          <div className={styles.detailText}>{parse(blogDetail.content)}</div>
+          <div>
+            <Link href="/blog" className={styles.detailBack}>
+              <ArrowUturnLeftIcon className="w-5 h-4 m-0 p-0" />
+              <span>戻る</span>
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,12 +3,25 @@ import { client, getBlogs } from "@/libs/microcms";
 import { BlogPostType } from "@/types/blogPost";
 import Link from "next/link";
 import BlogListCard from "../components/BlogListCard";
+import styles from "../styles/page.module.css";
+
+import { M_PLUS_Rounded_1c } from "next/font/google";
+
+const mPlus400 = M_PLUS_Rounded_1c({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default async function BlogList() {
   const { contents } = await getBlogs();
   console.log(contents);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h2 className={styles.blogListTitle}>
+        <span className={mPlus400.className}>記事一覧</span>
+      </h2>
       <ul>
         {contents.map((postData: BlogPostType) => {
           return (
