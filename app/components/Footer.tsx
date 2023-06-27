eb6a150d622849a2bdedc8da36369ad8c582b102
bugCreator6999 <bugcreator@bugcreators-cafe.com>
2023-06-27 18:52:07 +0900

    add header and footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "../styles/page.module.css";
+import { Montserrat_Alternates } from "next/font/google";
+
+const gFont = Montserrat_Alternates({
+  weight: ["600"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const Footer = () => {
-  return <div>Footer...</div>;
+  return (
+    <div className={styles.footer}>
+      <span className={gFont.className}>
+        (c) bugcreatos-tech / bugcreatos6999
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
 import React from "react";
+import styles from "../styles/page.module.css";
+import { M_PLUS_Rounded_1c } from "next/font/google";
+
+const mPlus400 = M_PLUS_Rounded_1c({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">top</Link>
-      <Link href="/blog">blog</Link>
-      <Link href="/about">about</Link>
+    <div className={styles.navbar}>
+      <Link href="/" className={`${styles.navText} ${styles.firstNav}`}>
+        <span className={mPlus400.className}>トップページ</span>
+      </Link>
+      <Link href="/blog" className={styles.navText}>
+        <span className={mPlus400.className}>ブログ</span>
+      </Link>
+      <Link href="/about" className={`${styles.navText} ${styles.lastNav}`}>
+        <span className={mPlus400.className}>このサイトについて</span>
+      </Link>
     </div>
   );
 };
